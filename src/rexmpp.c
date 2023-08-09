defanor/rexmpp

commit 938963c1d1c6a9b929d28a90030332d4f397ca61

Use timespec and monotonic clock for timers

src/rexmpp.c
@@ -578,7 +578,7 @@ rexmpp_err_t rexmpp_init (rexmpp_t *s,
   s->iq_cache = NULL;
   s->reconnect_number = 0;
   s->next_reconnect_time.tv_sec = 0;
-  s->next_reconnect_time.tv_usec = 0;
+  s->next_reconnect_time.tv_nsec = 0;
   s->initial_jid.full[0] = '\0';
   s->assigned_jid.full[0] = '\0';
   s->stanza_queue_size = 1024;
@@ -867,7 +867,7 @@ void rexmpp_schedule_reconnect (rexmpp_t *s) {
   if (seconds > 3600) {
     seconds = 3600;
   }
-  gettimeofday(&(s->next_reconnect_time), NULL);
+  clock_gettime(CLOCK_MONOTONIC, &(s->next_reconnect_time));
   s->next_reconnect_time.tv_sec += seconds;
   rexmpp_log(s, LOG_DEBUG, "Scheduled reconnect number %d, in %d seconds",
              s->reconnect_number,
@@ -2495,8 +2495,8 @@ rexmpp_err_t rexmpp_stop (rexmpp_t *s) {
 }
 
 rexmpp_err_t rexmpp_run (rexmpp_t *s, fd_set *read_fds, fd_set *write_fds) {
-  struct timeval now;
-  if (gettimeofday(&now, NULL) != 0) {
+  struct timespec now;
+  if (clock_gettime(CLOCK_MONOTONIC, &now) != 0) {
     rexmpp_log(s, LOG_ERR, "Failed to get time: %s", strerror(errno));
     return REXMPP_E_OTHER;
   }
@@ -2772,11 +2772,11 @@ int rexmpp_fds (rexmpp_t *s, fd_set *read_fds, fd_set *write_fds) {
   return max_fd;
 }
 
-struct timeval *rexmpp_timeout (rexmpp_t *s,
-                                struct timeval *max_tv,
-                                struct timeval *tv)
+struct timespec *rexmpp_timeout (rexmpp_t *s,
+                                 struct timespec *max_tv,
+                                 struct timespec *tv)
 {
-  struct timeval *ret = max_tv;
+  struct timespec *ret = max_tv;
 
   if (s->resolver_state != REXMPP_RESOLVER_NONE &&
       s->resolver_state != REXMPP_RESOLVER_READY) {
@@ -2787,14 +2787,14 @@ struct timeval *rexmpp_timeout (rexmpp_t *s,
 
   ret = rexmpp_jingle_timeout(s, ret, tv);
 
-  struct timeval now;
-  gettimeofday(&now, NULL);
+  struct timespec now;
+  clock_gettime(CLOCK_MONOTONIC, &now);
   if (s->reconnect_number > 0 &&
       s->next_reconnect_time.tv_sec > now.tv_sec &&
       (ret == NULL ||
        s->next_reconnect_time.tv_sec - now.tv_sec < ret->tv_sec)) {
     tv->tv_sec = s->next_reconnect_time.tv_sec - now.tv_sec;
-    tv->tv_usec = 0;
+    tv->tv_nsec = 0;
     ret = tv;
   }
 
@@ -2803,20 +2803,20 @@ struct timeval *rexmpp_timeout (rexmpp_t *s,
     time_t next_ping = s->last_network_activity + s->ping_delay - now.tv_sec;
     if (ret == NULL || next_ping < ret->tv_sec) {
       tv->tv_sec = next_ping;
-      tv->tv_usec = 0;
+      tv->tv_nsec = 0;
       ret = tv;
     }
   }
 
 #ifdef HAVE_CURL
-  long curl_timeout;
+  long curl_timeout;            /* in milliseconds */
   curl_multi_timeout(s->curl_multi, &curl_timeout);
   if (curl_timeout >= 0 &&
       (curl_timeout / 1000 < ret->tv_sec ||
        (curl_timeout / 1000 == ret->tv_sec &&
-        (curl_timeout % 1000) * 1000 < ret->tv_usec))) {
+        (curl_timeout % 1000) * 1000000 < ret->tv_nsec))) {
     tv->tv_sec = curl_timeout / 1000;
-    tv->tv_usec = (curl_timeout % 1000) * 1000;
+    tv->tv_nsec = (curl_timeout % 1000) * 1000000;
     ret = tv;
   }
 #endif

src/rexmpp.h
@@ -322,7 +322,7 @@ struct rexmpp
   /* Connection and stream management. */
   unsigned int reconnect_number;
   time_t reconnect_seconds;
-  struct timeval next_reconnect_time;
+  struct timespec next_reconnect_time;
   xmlNodePtr stanza_queue;
   uint32_t stanzas_out_count;
   uint32_t stanzas_out_acknowledged;
@@ -481,13 +481,13 @@ void rexmpp_iq_reply (rexmpp_t *s,
    @param[in] s ::rexmpp
    @param[in] max_tv An existing timeout (can be NULL), to return if
    there's no more urgent timeouts.
-   @param[in,out] tv An allocated timeval structure, to store the time
-   in.
+   @param[in,out] tv An allocated timespec structure, to store the
+   time in.
    @returns A pointer to either max_tv or tv.
 */
-struct timeval *rexmpp_timeout (rexmpp_t *s,
-                                struct timeval *max_tv,
-                                struct timeval *tv);
+struct timespec *rexmpp_timeout (rexmpp_t *s,
+                                 struct timespec *max_tv,
+                                 struct timespec *tv);
 
 /**
    @brief Sets file descriptors to watch.

emacs/xml_interface.c
@@ -329,8 +329,10 @@ int main (int argc, char **argv) {
 
   fd_set read_fds, write_fds;
   int nfds;
-  struct timeval tv;
-  struct timeval *mtv;
+  struct timespec tv;
+  struct timespec *mtv;
+  struct timeval tv_ms;
+  struct timeval *mtv_ms;
   int n = 0;
 
   do {
@@ -367,7 +369,13 @@ int main (int argc, char **argv) {
     FD_ZERO(&read_fds);
     FD_ZERO(&write_fds);
     nfds = rexmpp_fds(&s, &read_fds, &write_fds);
-    mtv = rexmpp_timeout(&s, NULL, (struct timeval*)&tv);
+    mtv = rexmpp_timeout(&s, NULL, &tv);
+    mtv_ms = NULL;
+    if (mtv != NULL) {
+      tv_ms.tv_sec = mtv->tv_sec;
+      tv_ms.tv_usec = mtv->tv_nsec / 1000;
+      mtv_ms = &tv_ms;
+    }
 
     /* Add other file descriptors we are interested in, particularly
        stdin for user input. */
@@ -375,7 +383,7 @@ int main (int argc, char **argv) {
 
     /* Run select(2) with all those file descriptors and timeouts,
        waiting for either user input or some rexmpp event to occur. */
-    n = select(nfds, &read_fds, &write_fds, NULL, mtv);
+    n = select(nfds, &read_fds, &write_fds, NULL, mtv_ms);
     if (n == -1) {
       printf("select error: %s\n", strerror(errno));
       break;

examples/basic.c
@@ -11,6 +11,7 @@
 #include <errno.h>
 #include <syslog.h>
 #include <gsasl.h>
+#include <time.h>
 #include <rexmpp.h>
 #include <rexmpp_sasl.h>
 
@@ -155,8 +156,10 @@ int main (int argc, char **argv) {
 
   fd_set read_fds, write_fds;
   int nfds;
-  struct timeval tv;
-  struct timeval *mtv;
+  struct timespec tv;
+  struct timespec *mtv;
+  struct timeval tv_ms;
+  struct timeval *mtv_ms;
   int n = 0;
 
   do {
@@ -217,7 +220,13 @@ int main (int argc, char **argv) {
     FD_ZERO(&read_fds);
     FD_ZERO(&write_fds);
     nfds = rexmpp_fds(&s, &read_fds, &write_fds);
-    mtv = rexmpp_timeout(&s, NULL, (struct timeval*)&tv);
+    mtv = rexmpp_timeout(&s, NULL, &tv);
+    mtv_ms = NULL;
+    if (mtv != NULL) {
+      tv_ms.tv_sec = mtv->tv_sec;
+      tv_ms.tv_usec = mtv->tv_nsec / 1000;
+      mtv_ms = &tv_ms;
+    }
 
     /* Add other file descriptors we are interested in, particularly
        stdin for user input. */
@@ -225,7 +234,7 @@ int main (int argc, char **argv) {
 
     /* Run select(2) with all those file descriptors and timeouts,
        waiting for either user input or some rexmpp event to occur. */
-    n = select(nfds, &read_fds, &write_fds, NULL, mtv);
+    n = select(nfds, &read_fds, &write_fds, NULL, mtv_ms);
     if (n == -1) {
       printf("select error: %s\n", strerror(errno));
       break;

src/rexmpp_dns.c
@@ -181,16 +181,30 @@ int rexmpp_dns_fds (rexmpp_t *s, fd_set *read_fds, fd_set *write_fds) {
 #endif
 }
 
-struct timeval * rexmpp_dns_timeout (rexmpp_t *s,
-                                     struct timeval *max_tv,
-                                     struct timeval *tv)
+struct timespec * rexmpp_dns_timeout (rexmpp_t *s,
+                                      struct timespec *max_tv,
+                                      struct timespec *tv)
 {
 #if defined(USE_UNBOUND)
   (void)s;
   (void)tv;
   return max_tv;
 #elif defined(USE_CARES)
-  return ares_timeout(s->resolver.channel, max_tv, tv);
+  struct timeval tv_ms;
+  struct timeval *max_tv_ms = NULL;
+  if (max_tv != NULL) {
+    max_tv_ms = &tv_ms;
+    tv_ms.tv_sec = tv->tv_sec;
+    tv_ms.tv_usec = tv->tv_nsec / 1000;
+  }
+  struct timeval *ret_ms = ares_timeout(s->resolver.channel, max_tv_ms, &tv_ms);
+  if (ret_ms == max_tv_ms) {
+    return max_tv;
+  } else {
+    tv->tv_sec = tv_ms.tv_sec;
+    tv->tv_nsec = tv_ms.tv_usec * 1000;
+    return tv;
+  }
 #else
   (void)s;
   (void)max_tv;

src/rexmpp_dns.h
@@ -103,9 +103,9 @@ int rexmpp_dns_fds (rexmpp_t *s, fd_set *read_fds, fd_set *write_fds);
 /**
    @brief Reports timeouts.
 */
-struct timeval * rexmpp_dns_timeout (rexmpp_t *s,
-                                     struct timeval *max_tv,
-                                     struct timeval *tv);
+struct timespec * rexmpp_dns_timeout (rexmpp_t *s,
+                                      struct timespec *max_tv,
+                                      struct timespec *tv);
 
 typedef void (*dns_query_cb_t) (rexmpp_t *s, void *ptr, rexmpp_dns_result_t *result);
 

src/rexmpp_jingle.c
@@ -1779,9 +1779,9 @@ int rexmpp_jingle_fds(rexmpp_t *s, fd_set *read_fds, fd_set *write_fds) {
   return (nfds + 1);
 }
 
-struct timeval * rexmpp_jingle_timeout (rexmpp_t *s,
-                                        struct timeval *max_tv,
-                                        struct timeval *tv) {
+struct timespec * rexmpp_jingle_timeout (rexmpp_t *s,
+                                         struct timespec *max_tv,
+                                         struct timespec *tv) {
 #ifdef ENABLE_CALLS
   gint poll_timeout;
   GPollFD poll_fds[10];
@@ -1811,12 +1811,12 @@ struct timeval * rexmpp_jingle_timeout (rexmpp_t *s,
 
     if (poll_timeout >= 0) {
       int sec = poll_timeout / 1000;
-      int usec = (poll_timeout % 1000) * 1000;
+      int nsec = (poll_timeout % 1000) * 1000000;
       if (max_tv == NULL ||
           (max_tv->tv_sec > sec ||
-           (max_tv->tv_sec == sec && max_tv->tv_usec > usec))) {
+           (max_tv->tv_sec == sec && max_tv->tv_nsec > nsec))) {
         tv->tv_sec = sec;
-        tv->tv_usec = usec;
+        tv->tv_nsec = nsec;
         max_tv = tv;
       }
     }

src/rexmpp_jingle.h
@@ -121,9 +121,9 @@ struct rexmpp_jingle_ctx {
 
 int rexmpp_jingle_init (rexmpp_t *s);
 rexmpp_err_t rexmpp_jingle_run (rexmpp_t *s, fd_set *read_fds, fd_set *write_fds);
-struct timeval * rexmpp_jingle_timeout (rexmpp_t *s,
-                                        struct timeval *max_tv,
-                                        struct timeval *tv);
+struct timespec * rexmpp_jingle_timeout (rexmpp_t *s,
+                                         struct timespec *max_tv,
+                                         struct timespec *tv);
 int rexmpp_jingle_fds(rexmpp_t *s, fd_set *read_fds, fd_set *write_fds);
 
 rexmpp_err_t

commit 116c8f7c3a092d192b44ba6eea53566eceb656b9

Fix pinging
It was broken since commit 938963c1d1c6a9b929d28a90030332d4f397ca61,
since seconds from monotonic clocks were compared to calendar
ones. Now last_network_activity is a timespec, using monotonic clocks
as well.

src/rexmpp.c
@@ -562,7 +562,8 @@ rexmpp_err_t rexmpp_init (rexmpp_t *s,
   s->console_print_cb = NULL;
   s->ping_delay = 600;
   s->ping_requested = 0;
-  s->last_network_activity = 0;
+  s->last_network_activity.tv_sec = 0;
+  s->last_network_activity.tv_nsec = 0;
   s->disco_info = NULL;
 
   /* The default description. Since the players and streamers are
@@ -933,7 +934,7 @@ rexmpp_err_t rexmpp_send_continue (rexmpp_t *s)
                   0);
     }
     if (ret > 0) {
-      s->last_network_activity = time(NULL);
+      clock_gettime(CLOCK_MONOTONIC, &(s->last_network_activity));
       s->send_buffer_sent += ret;
       if (s->send_buffer_sent == s->send_buffer_len) {
         free(s->send_buffer);
@@ -1221,7 +1222,7 @@ rexmpp_err_t rexmpp_recv (rexmpp_t *s) {
       chunk_raw_len = recv(s->server_socket, chunk_raw, 4096, 0);
     }
     if (chunk_raw_len > 0) {
-      s->last_network_activity = time(NULL);
+      clock_gettime(CLOCK_MONOTONIC, &(s->last_network_activity));
       if (s->sasl_state == REXMPP_SASL_ACTIVE) {
         sasl_err = rexmpp_sasl_decode(s, chunk_raw, chunk_raw_len,
                                       &chunk, &chunk_len);
@@ -2493,7 +2494,7 @@ rexmpp_err_t rexmpp_run (rexmpp_t *s, fd_set *read_fds, fd_set *write_fds) {
 
   /* Pinging the server. */
   if (s->tcp_state == REXMPP_TCP_CONNECTED &&
-      s->last_network_activity + s->ping_delay <= time(NULL)) {
+      s->last_network_activity.tv_sec + s->ping_delay <= now.tv_sec) {
     if (s->ping_requested == 0) {
       s->ping_requested = 1;
       rexmpp_xml_t *ping_cmd =
@@ -2653,8 +2654,9 @@ struct timespec *rexmpp_timeout (rexmpp_t *s,
   }
 
   if (s->tcp_state == REXMPP_TCP_CONNECTED &&
-      s->last_network_activity + s->ping_delay > now.tv_sec) {
-    time_t next_ping = s->last_network_activity + s->ping_delay - now.tv_sec;
+      s->last_network_activity.tv_sec + s->ping_delay > now.tv_sec) {
+    time_t next_ping =
+      s->last_network_activity.tv_sec + s->ping_delay - now.tv_sec;
     if (ret == NULL || next_ping < ret->tv_sec) {
       tv->tv_sec = next_ping;
       tv->tv_nsec = 0;

src/rexmpp.h
@@ -333,7 +333,7 @@ struct rexmpp
   /* Server ping configuration and state. */
   int ping_delay;
   int ping_requested;
-  time_t last_network_activity;
+  struct timespec last_network_activity;
 
   /* DNS-related structures. */
   rexmpp_dns_ctx_t resolver;

src/rexmpp.rs
@@ -200,7 +200,7 @@ pub struct Rexmpp {
     // Server ping configuration and state
     pub ping_delay: c_int,
     pub ping_requested: c_int,
-    pub last_network_activity: time_t,
+    pub last_network_activity: timespec,
 
     // DNS-related structures
     pub resolver: *mut c_void,